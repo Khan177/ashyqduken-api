1d1c19d271bd76382fdb7903c1a99b11616c8b9d
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2020-07-18 14:02:34 +0600

    initial

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const products = require("./products");
+const PORT = process.env.PORT;
 console.log(products);
-app.get("/url", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.json(products);
   console.log(products);
 });
-app.listen(3030, () => {
-  console.log(parseInt("55058"));
-});
+app.listen(PORT);
