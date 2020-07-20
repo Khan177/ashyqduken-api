9b52d9831236d141e1a8b7f1ac56b92852290f5b
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2020-07-20 20:03:41 +0600

    adding cors header

--- main.js
+++ main.js
@@ -6,20 +6,24 @@ const products = require("./products").allItems;
 const categories = require("./categories").categories;
 const PORT = process.env.PORT;
 console.log(products);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
   res.json(products);
 });
 app.get("/phones", (req, res, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
   res.json(phones);
 });
 app.get("/laptops", (req, res, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
   res.json(laptops);
 });
 app.get("/categories", (req, res, next) => {
-  response.header("Access-Control-Allow-Origin", "*");
   res.json(categories);
 });
 app.listen(PORT);
