7e56651467dd2fe6e98cf4b3961a9ccc478fd9bd
Adilkhan Nurgaliyev <khan17171717@gmail.com>
2020-07-20 19:20:21 +0600

    adding categories

---
+++ categories.js
@@ -0,0 +1,19 @@
+const categories = [
+  {
+    id: 0,
+    name: "Smartfon",
+    path: "phones",
+    image:
+      "https://fdn.gsmarena.com/imgroot/news/19/12/top-phones-of-2019/-727/gsmarena_001.jpg",
+  },
+  {
+    id: 1,
+    name: "Noýtbýk",
+    path: "laptops",
+    image: "https://nout.kz/upload/medialibrary/8c5/unnamed.0.0.jpg",
+  },
+];
+
+module.exports = {
+  categories: categories,
+};
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const phones = require("./products").phones;
 const laptops = require("./products").laptops;
 const products = require("./products").allItems;
+const categories = require("./categories").categories;
 const PORT = process.env.PORT;
 console.log(products);
 app.get("/", (req, res, next) => {
@@ -13,5 +14,8 @@ app.get("/phones", (req, res, next) => {
 });
 app.get("/laptops", (req, res, next) => {
   res.json(laptops);
+});
+app.get("/categories", (req, res, next) => {
+  res.json(categories);
 });
 app.listen(PORT);
